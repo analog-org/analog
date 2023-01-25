2c0b0fb24c90521b1387dc9bd3510a46f5cd8ca1
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-25 16:21:26 +0000

    replaced with another flowbit component

--- apps/dashboard/src/components/Footer.tsx
+++ apps/dashboard/src/components/Footer.tsx
@@ -8,14 +8,43 @@ import { useSession, signIn, signOut } from "next-auth/react";
 const MainFooter: NextPage = () => {
   return (
     <div>
-      <Footer container={true}>
-        <Footer.Copyright href="#" by="Analog" year={new Date().getFullYear()} />
-        <Footer.LinkGroup>
-          <Footer.Link href="#">About</Footer.Link>
-          <Footer.Link href="#">Privacy Policy</Footer.Link>
-          <Footer.Link href="#">Licensing</Footer.Link>
-          <Footer.Link href="#">Discord</Footer.Link>
-        </Footer.LinkGroup>
+      <Footer container={true} className="rounded-none">
+        <div className="w-full">
+          <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+            <div>
+              <Footer.Brand
+                href="https://flowbite.com"
+                src="https://flowbite.com/docs/images/logo.svg"
+                alt="Flowbite Logo"
+                name="Flowbite"
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+              <div>
+                <Footer.Title title="about" />
+                <Footer.LinkGroup col={true}>
+                  <Footer.Link href="#">Flowbite</Footer.Link>
+                  <Footer.Link href="#">Tailwind CSS</Footer.Link>
+                </Footer.LinkGroup>
+              </div>
+              <div>
+                <Footer.Title title="Follow us" />
+                <Footer.LinkGroup col={true}>
+                  <Footer.Link href="#">Github</Footer.Link>
+                  <Footer.Link href="#">Discord</Footer.Link>
+                </Footer.LinkGroup>
+              </div>
+              <div>
+                <Footer.Title title="Legal" />
+                <Footer.LinkGroup col={true}>
+                  <Footer.Link href="#">Privacy Policy</Footer.Link>
+                  <Footer.Link href="#">Terms & Conditions</Footer.Link>
+                </Footer.LinkGroup>
+              </div>
+            </div>
+          </div>
+          <Footer.Divider />
+        </div>
       </Footer>
     </div>
   );
