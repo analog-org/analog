39f7e449e436b816bc3af9f811c019d741484067
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-25 16:24:08 +0000

    removed useless links and added analog branding

--- apps/dashboard/src/components/Footer.tsx
+++ apps/dashboard/src/components/Footer.tsx
@@ -13,32 +13,22 @@ const MainFooter: NextPage = () => {
           <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
             <div>
               <Footer.Brand
-                href="https://flowbite.com"
-                src="https://flowbite.com/docs/images/logo.svg"
+                href="https://github.com/analog-org"
+                src="https://media.discordapp.net/attachments/1037032323276877945/1065094779509166081/Analog.png?width=850&height=850"
                 alt="Flowbite Logo"
-                name="Flowbite"
+                name="Analog"
               />
             </div>
             <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
               <div>
-                <Footer.Title title="about" />
-                <Footer.LinkGroup col={true}>
-                  <Footer.Link href="#">Flowbite</Footer.Link>
-                  <Footer.Link href="#">Tailwind CSS</Footer.Link>
-                </Footer.LinkGroup>
-              </div>
-              <div>
                 <Footer.Title title="Follow us" />
                 <Footer.LinkGroup col={true}>
-                  <Footer.Link href="#">Github</Footer.Link>
-                  <Footer.Link href="#">Discord</Footer.Link>
-                </Footer.LinkGroup>
-              </div>
-              <div>
-                <Footer.Title title="Legal" />
-                <Footer.LinkGroup col={true}>
-                  <Footer.Link href="#">Privacy Policy</Footer.Link>
-                  <Footer.Link href="#">Terms & Conditions</Footer.Link>
+                  <Footer.Link href="https://github.com/analog-org">
+                    Github
+                  </Footer.Link>
+                  <Footer.Link href="https://discord.gg/XsXC3pAhBY">
+                    Discord
+                  </Footer.Link>
                 </Footer.LinkGroup>
               </div>
             </div>
