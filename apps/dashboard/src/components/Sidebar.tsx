f4f554d495e89a8f1216099cc9c30cb1ce41c736
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-28 21:42:20 +0000

    removed redundant aside component

--- apps/dashboard/src/components/Sidebar.tsx
+++ apps/dashboard/src/components/Sidebar.tsx
@@ -11,25 +11,23 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 const SideBar: NextPage = () => {
   return (
-    <aside className="h-full">
-      <div className="w-fit">
-        <Sidebar aria-label="Sidebar with logo branding example">
-          <Sidebar.Logo
-            href="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
-            img="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
-            imgAlt="Analog logo"
-          >
-            Analog
-          </Sidebar.Logo>
-          <Sidebar.Items>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item href="#">Dashboard</Sidebar.Item>
-              <Sidebar.Item href="/embed">Embed Builder</Sidebar.Item>
-            </Sidebar.ItemGroup>
-          </Sidebar.Items>
-        </Sidebar>
-      </div>
-    </aside>
+    <div className="w-fit">
+      <Sidebar aria-label="Sidebar with logo branding example">
+        <Sidebar.Logo
+          href="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
+          img="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
+          imgAlt="Analog logo"
+        >
+          Analog
+        </Sidebar.Logo>
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item href="#">Dashboard</Sidebar.Item>
+            <Sidebar.Item href="./embed">Embed Builder</Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </div>
   );
 };
 
