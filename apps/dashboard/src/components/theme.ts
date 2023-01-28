e5f43e3065dd27e4cdcf1a17cc566b9001eb2d9b
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-28 20:43:12 +0000

    changed sidebar color

--- apps/dashboard/src/components/theme.ts
+++ apps/dashboard/src/components/theme.ts
@@ -1,4 +1,7 @@
 import { CustomFlowbiteTheme } from "flowbite-react/lib/esm/components/Flowbite/FlowbiteTheme";
 
 export const customtheme: CustomFlowbiteTheme = {
+  sidebar: {
+    inner: 'h-full overflow-y-auto overflow-x-hidden rounded bg-zinc-900 py-4 px-3 '
+  }
 };
