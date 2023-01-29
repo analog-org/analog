fb87adfb8f1103af7a780204666ff6a52d181200
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-29 21:06:34 +0000

    changed sidebar item hover effect

--- apps/dashboard/src/components/theme.ts
+++ apps/dashboard/src/components/theme.ts
@@ -2,6 +2,9 @@ import { CustomFlowbiteTheme } from "flowbite-react/lib/esm/components/Flowbite/
 
 export const customtheme: CustomFlowbiteTheme = {
   sidebar: {
-    inner: 'h-full overflow-y-auto overflow-x-hidden rounded bg-zinc-900 py-4 px-3 '
+    inner: 'h-full overflow-y-auto overflow-x-hidden bg-zinc-900 py-4 px-3 ',
+    item: {
+      base: 'flex items-center justify-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-zinc-800'
+    }
   }
 };
