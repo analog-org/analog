6d8738262f45e3cb0337e03d15fa03bc4b95a2df
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-30 16:45:28 +0000

    client info

---
+++ apps/dashboard/src/pages/api/discord/clientInfo.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { FastAverageColor } from 'fast-average-color';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+  const botInfoFetch = await fetch(
+    `https://discord.com/api/v10/users/@me`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  );
+  const botInfo = await botInfoFetch.json();
+
+  /* const fac = new FastAverageColor();
+  
+  const accentColor = (await fac.getColorAsync(`https://cdn.discordapp.com/avatars/${botInfo.id}/${botInfo.avatar}.png`)) */
+
+  res.status(200).json({botInfo});
+}
+
+
