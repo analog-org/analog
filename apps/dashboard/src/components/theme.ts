f899d1b7deb4ee9d1bab47240ab8fcfff86348ea
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-01-31 15:02:58 +0000

    rounded analog logo & sidebar fills entire side of screen

--- apps/dashboard/src/components/theme.ts
+++ apps/dashboard/src/components/theme.ts
@@ -2,9 +2,13 @@ import { CustomFlowbiteTheme } from "flowbite-react/lib/esm/components/Flowbite/
 
 export const customtheme: CustomFlowbiteTheme = {
   sidebar: {
-    inner: 'h-full overflow-y-auto overflow-x-hidden bg-zinc-900 py-4 px-3 ',
+    inner: 'h-screen overflow-y-auto overflow-x-hidden bg-zinc-900 py-4 px-3 ',
     item: {
       base: 'flex items-center justify-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-zinc-800'
+    },
+    logo: {
+      img: 'mr-3 h-6 rounded-full sm:h-7'
     }
+
   }
 };
