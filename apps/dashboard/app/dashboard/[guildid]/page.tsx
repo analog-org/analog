4f70a1af473bf01b549790431c721768a336cde3
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-09 16:40:16 +0000

    testing file syncing with the page

--- apps/dashboard/app/dashboard/[guildid]/page.tsx
+++ apps/dashboard/app/dashboard/[guildid]/page.tsx
@@ -1,8 +1,22 @@
 import { NextPage } from "next";
+import path from "node:path";
 import { ReactNode } from "react";
+import fs, { read, readdirSync } from "fs";
 
-export default function Home({ params }) {
+
+
+export default async function Home({ params }) {
+  const filelist = path.join(process.cwd(), "app/dashboard/(Guild)");
+  const componentFiles = fs.readdirSync(filelist).filter((file) => file.endsWith(".tsx"));
+  const components = []
+
+  for (const file of componentFiles) {
+    const component = path.join(filelist, file);
+    components.push(component.toString());
+  }
+  console.log(components)
+
   const { guildid } = params;
 
-  return <div className="text-white">Guild Id: {guildid}</div>;
+  return <div className="text-white">Guild Id: {guildid}{filelist}</div>;
 }
