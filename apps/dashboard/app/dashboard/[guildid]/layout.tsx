cd7566d083c4437a2050251849b860ba0794b3a6
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-10 16:56:00 +0000

    imported bot guilds fetch from dashboard page

--- apps/dashboard/app/dashboard/[guildid]/layout.tsx
+++ apps/dashboard/app/dashboard/[guildid]/layout.tsx
@@ -4,7 +4,11 @@ import SideBar from "./Sidebar";
 import { ReactNode } from "react";
 import DashNavbar from "./DashNav";
 import path from "node:path";
+import { authOptions } from "../../../src/pages/api/auth/[...nextauth]";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import fs, { read, readdirSync } from "fs";
+import perms from "../../../src/utils/bitfield";
 
 const filelist = path.join(process.cwd(), "app/dashboard/[guildid]");
 const componentFiles = fs
@@ -17,6 +21,37 @@ for (const file of componentFiles) {
 
 export default async function DashboardLayout({ children, params }) {
   const { guildid } = params;
+
+  const session = await getServerSession(authOptions);
+
+  const guildFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    }
+  );
+  const guilds = await guildFetch.json();
+  
+  const botGuildsFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  );
+  const botGuilds = await botGuildsFetch.json();
+
+  guilds.map((gld: guild) => {
+    const serverPerms = perms(gld.permissions);
+    if (serverPerms.includes("MANAGE_GUILD")) {
+
+    }
+  });
 
   return (
     <div className="bg-zinc-800">
