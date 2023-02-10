18c0c5b5f3a9745b1821ae48e562c80ec94de7a5
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-10 15:55:33 +0000

    removed filesync

--- apps/dashboard/app/dashboard/[guildid]/page.tsx
+++ apps/dashboard/app/dashboard/[guildid]/page.tsx
@@ -6,17 +6,9 @@ import fs, { read, readdirSync } from "fs";
 
 
 export default async function Home({ params }) {
-  const filelist = path.join(process.cwd(), "app/dashboard/(Guild)");
-  const componentFiles = fs.readdirSync(filelist).filter((file) => file.endsWith(".tsx"));
-  const components = []
-
-  for (const file of componentFiles) {
-    const component = path.join(filelist, file);
-    components.push(component.toString());
-  }
-  console.log(components)
+  
 
   const { guildid } = params;
 
-  return <div className="text-white">Guild Id: {guildid}{filelist}</div>;
+  return <div className="text-white">Guild Id: {guildid}</div>;
 }
