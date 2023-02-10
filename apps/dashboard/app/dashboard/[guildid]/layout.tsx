cd3274a1dd048c2ce69bc038babf13a82c1aed19
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-10 15:55:21 +0000

    removed useless typing and added params

--- apps/dashboard/app/dashboard/[guildid]/layout.tsx
+++ apps/dashboard/app/dashboard/[guildid]/layout.tsx
@@ -3,17 +3,26 @@ import NavBar from "../../Navbar";
 import SideBar from "./Sidebar";
 import { ReactNode } from "react";
 import DashNavbar from "./DashNav";
+import path from "node:path";
+import fs, { read, readdirSync } from "fs";
 
-type LayoutProps = {
-  children: React.ReactNode;
-}
 
-export default function DashboardLayout ({ children }: LayoutProps){
+
+export default function DashboardLayout ({ children, params }){
+  const { guildid } = params;
+  const filelist = path.join(process.cwd(), "app/dashboard/[guildid]");
+  const componentFiles = fs.readdirSync(filelist)
+  const components = []
+
+  for (const file of componentFiles) {
+    components.push(file);
+  }
+  
   return (
     <div className="bg-zinc-800">
       <DashNavbar />
       <div className="flex">
-        <SideBar />
+        <SideBar pages={components} id={guildid} />
         <div className="flex-grow p-6">{children}</div>
       </div>
     </div>
