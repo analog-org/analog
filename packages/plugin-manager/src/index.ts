c484d23124006a3c68af43ad64e59f49ecdd88fa
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-22 15:16:34 +0000

    properly returns plugin list

--- packages/plugin-manager/src/index.ts
+++ packages/plugin-manager/src/index.ts
@@ -2,9 +2,9 @@ import fs from "fs";
 import path from "path";
 
 export function pluginList() {
-  const pluginDir = path.join(__dirname, "../plugins");
+  const pluginDir = path.join(__dirname, "../../../plugins");
   const plugins = fs.readdirSync(pluginDir);
-  const pluginList = plugins.map((plugin) => {
-    return plugin.toString();
-  });
+  let pluginlist: any[] = []
+  plugins.map((plugin) => pluginlist.push(plugin));
+  return pluginlist;
 }
