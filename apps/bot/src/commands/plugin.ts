99b3beac919675f5afea18d95fcad44f0c69efdd
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-22 15:16:03 +0000

    changed logger

--- apps/bot/src/commands/plugin.ts
+++ apps/bot/src/commands/plugin.ts
@@ -5,7 +5,7 @@ import { pluginList } from 'plugin-manager';
 
 @ApplyOptions<Subcommand.Options>({
 	description: 'For managing the plugin',
-	subcommands: [{ name: 'list', chatInputRun: 'listPlugins',  }]
+	subcommands: [{ name: 'list', chatInputRun: 'listPlugins' }]
 })
 export class UserCommand extends Subcommand {
 	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
@@ -18,14 +18,13 @@ export class UserCommand extends Subcommand {
 	}
 
 	public async listPlugins(interaction: Subcommand.ChatInputCommandInteraction) {
-		
+		const { client, logger } = this.container;
+		logger.info(pluginList())
+
 		return await interaction.reply({ content: 'plugins listed in console' });
-		try{
-			this.container.logger.info(pluginList());
-		} catch (error) {
-			this.container.logger.fatal(error);
-			
-		}
+
+		
+
 		
 	}
 }
