42c48d1821109cb0f751bb33a9299e4121ce8709
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-23 15:15:05 +0000

    boilerplate

---
+++ apps/bot/src/interaction-handlers/autocomplete/pluginList.ts
@@ -0,0 +1,29 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
+import type { AutocompleteInteraction } from 'discord.js';
+import { pluginList } from 'plugin-manager';
+
+@ApplyOptions<InteractionHandler.Options>({
+  interactionHandlerType: InteractionHandlerTypes.Autocomplete
+})
+export class AutocompleteHandler extends InteractionHandler {
+  public override async run(interaction: AutocompleteInteraction, result: InteractionHandler.ParseResult<this>) {
+    return interaction.respond(result);
+  }
+
+  public override async parse(interaction: AutocompleteInteraction) {
+    if (interaction.commandName !== 'plugin') return this.none();
+
+    const focusedOption = interaction.options.getFocused(true);
+
+    switch (focusedOption.name) {
+      case 'name': {
+        const plugins = pluginList()
+
+        return this.some();
+      }
+      default:
+        return this.none();
+    }
+  }
+}
