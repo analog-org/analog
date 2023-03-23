5291faa78b26e4b4c8ffa0480c6976d9572e342d
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-23 15:23:30 +0000

    outlined logic

--- apps/bot/src/interaction-handlers/autocomplete/pluginList.ts
+++ apps/bot/src/interaction-handlers/autocomplete/pluginList.ts
@@ -4,26 +4,26 @@ import type { AutocompleteInteraction } from 'discord.js';
 import { pluginList } from 'plugin-manager';
 
 @ApplyOptions<InteractionHandler.Options>({
-  interactionHandlerType: InteractionHandlerTypes.Autocomplete
+	interactionHandlerType: InteractionHandlerTypes.Autocomplete
 })
 export class AutocompleteHandler extends InteractionHandler {
-  public override async run(interaction: AutocompleteInteraction, result: InteractionHandler.ParseResult<this>) {
-    return interaction.respond(result);
-  }
+	public override async run(interaction: AutocompleteInteraction, result: InteractionHandler.ParseResult<this>) {
+		return interaction.respond(result);
+	}
 
-  public override async parse(interaction: AutocompleteInteraction) {
-    if (interaction.commandName !== 'plugin') return this.none();
+	public override async parse(interaction: AutocompleteInteraction) {
+		if (interaction.commandName !== 'plugin') return this.none();
 
-    const focusedOption = interaction.options.getFocused(true);
+		const focusedOption = interaction.options.getFocused(true);
 
-    switch (focusedOption.name) {
-      case 'name': {
-        const plugins = pluginList()
+		switch (focusedOption.name) {
+			case 'name': {
+				const plugins = pluginList();
 
-        return this.some();
-      }
-      default:
-        return this.none();
-    }
-  }
+				return this.some(plugins.map((pl) => ({ name: pl.name, value: pl.name })));
+			}
+			default:
+				return this.none();
+		}
+	}
 }
